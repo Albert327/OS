7af1f377e6135d9a41aded58b801f074f0a0f29b
Aodong Liu <144232655+Albert327@users.noreply.github.com>
2025-05-21 16:20:18 +0800

    Update crepl.c

--- crepl.c
+++ crepl.c
@@ -1,84 +1,212 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
 #include <dlfcn.h>
 #include <sys/wait.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stdbool.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <ctype.h>
 
-static int wrapper_num;
-static char *wrapper_func[] = {
-	"int ", "__expr_wrapper_", "(){return ", ";}\n",
-};
-static char wrapper_filec[] = "/tmp/crepl_tmp.c";
-static char wrapper_fileso[] = "/tmp/crepl_tmp.so";
-static FILE *wrapper_fd;
-static char *const gcc_argv[] = {
-	"gcc",
-	"-fPIC",
-	"-shared",
-	wrapper_filec,
-	"-o",
-	wrapper_fileso,
-	NULL
-};
+#define MAX_DECLS 100
+#define TEMPLATE "/tmp/crepl-XXXXXX"
 
-void compile() {
-	if (fork() == 0) {
-		execvp("gcc", gcc_argv);
-		perror("gcc error");
-		exit(-1);
-	}
-	wait(NULL);
-}
+typedef struct {
+    char *proto;
+} Declaration;
 
-int main(int argc, char *argv[]) {
-	// empty the file
-	wrapper_fd = fopen(wrapper_filec, "w");
-	fclose(wrapper_fd);
+Declaration decls[MAX_DECLS]; 
+int decl_count = 0;          
+void *handles[100];           
+int handle_count = 0;
+static const int is_32bit = (sizeof(void*) == 4);       
 
-  static char line[4096];
-	void *handle;
-	int (*wrapper)();
-	char wrapper_buf[100];
-	
-  while (1) {
-    printf("crepl> ");
-    fflush(stdout);
-    if (!fgets(line, sizeof(line), stdin)) {
-			// read failed
-      break;
+void extract_prototype(const char *line, char **proto) {
+    char *start = strchr(line, ' ') + 1;
+    while (isspace(*start)) start++;
+    
+    char *name_end = strchr(start, '(');
+    if (!name_end) return;
+    size_t name_len = name_end - start;
+    char func_name[256] = {0};
+    strncpy(func_name, start, name_len > 255 ? 255 : name_len);
+
+    char *param_start = name_end + 1;
+    char *param_end = strchr(param_start, ')');
+    if (!param_end) return;
+    size_t params_len = param_end - param_start;
+    char params[512] = {0};
+    strncpy(params, param_start, params_len > 511 ? 511 : params_len);
+
+    int param_count = 0;
+    char *token = strtok(params, ",");
+    while (token) {
+        param_count++;
+        token = strtok(NULL, ",");
     }
 
-		int len = strlen(line);
-		if (len >= 3 && !strncmp(line, "int", 3)) {
-			// function define
-			wrapper_fd = fopen(wrapper_filec, "a");
-			fprintf(wrapper_fd, "\n%s\n", line);
-			fclose(wrapper_fd);
-		}
-		else {
-			// expression
-			wrapper_fd = fopen(wrapper_filec, "a");
-			fprintf(wrapper_fd, "%s%s%d%s%s%s",
-					wrapper_func[0], wrapper_func[1], wrapper_num, wrapper_func[2], line, wrapper_func[3]);
-			fclose(wrapper_fd);
+    char param_list[512] = {0};
+    for (int i = 0; i < param_count; i++) {
+        if (i > 0) strcat(param_list, ", ");
+        strcat(param_list, "int");
+    }
 
-			compile();
-			handle = dlopen(wrapper_fileso, RTLD_LAZY);
-			if (!handle) {
-				printf("handle error\n");
-				continue;
-			}
-			sprintf(wrapper_buf, "%s%d", wrapper_func[1], wrapper_num);
-			wrapper = (int (*)()) dlsym(handle, wrapper_buf);
-			if (!wrapper) {
-				printf("wrapper error\n");
-			}
-			else {
-				printf("%d\n", wrapper());
-			}
-			wrapper_num++;
-			dlclose(handle);
-		}
-  }
+    int needed = snprintf(NULL, 0, "int %s(%s);", func_name, param_list);
+    if (needed < 0) {
+        *proto = NULL;
+        return;
+    }
+    *proto = malloc(needed + 1);
+    if (!*proto) {
+        perror("malloc");
+        return;
+    }
+    snprintf(*proto, needed + 1, "int %s(%s);", func_name, param_list);
+}
+
+int compile_so(const char *c_path, const char *so_path) {
+    pid_t pid = fork();
+    if (pid == 0) {
+        if (is_32bit) {
+            execlp("gcc", "gcc", "-m32", "-shared", "-fPIC", "-o", so_path, c_path,
+                   "-Wl,--unresolved-symbols=ignore-all", NULL);
+        } else {
+            execlp("gcc", "gcc", "-shared", "-fPIC", "-o", so_path, c_path,
+                   "-Wl,--unresolved-symbols=ignore-all", NULL);
+        }
+        _exit(EXIT_FAILURE);
+    } else {
+        int status;
+        waitpid(pid, &status, 0);
+        return WIFEXITED(status) ? WEXITSTATUS(status) : -1;
+    }
+}
+
+void process_expression(const char *line) {
+    static int expr_id = 0;
+    expr_id++;
+
+    char c_path[] = TEMPLATE ".c";
+    int fd = mkstemps(c_path, 2);
+    if (fd == -1) {
+        perror("mkstemps");
+        return;
+    }
+
+    dprintf(fd, "// Auto-generated declarations\n");
+    for (int i = 0; i < decl_count; i++) {
+        dprintf(fd, "%s\n", decls[i].proto);
+    }
+
+    dprintf(fd, "int __expr_wrapper_%d() { return %s; }\n", expr_id, line);
+    close(fd);
+
+    char so_path[256];
+    size_t len = strlen(c_path);
+    if (len >= 2 && strcmp(c_path + len - 2, ".c") == 0) {
+        snprintf(so_path, sizeof(so_path), "%.*s.so", (int)(len - 2), c_path);
+    } else {
+        snprintf(so_path, sizeof(so_path), "%s.so", c_path);
+    }
+
+    if (compile_so(c_path, so_path) != 0) {
+        fprintf(stderr, "Compilation failed\n");
+        unlink(c_path);
+        return;
+    }
+
+    void *handle = dlopen(so_path, RTLD_NOW);
+    if (!handle) {
+        fprintf(stderr, "dlopen error: %s\n", dlerror());
+        unlink(c_path);
+        unlink(so_path);
+        return;
+    }
+
+    char func_name[64];
+    snprintf(func_name, sizeof(func_name), "__expr_wrapper_%d", expr_id);
+    int (*func)(void) = dlsym(handle, func_name);
+    if (func) {
+        printf("= %d\n", func());
+    } else {
+        fprintf(stderr, "dlsym error: %s\n", dlerror());
+    }
+
+    dlclose(handle);
+    unlink(c_path);
+    unlink(so_path);
+}
+
+void add_declaration(const char *proto) {
+    if (decl_count >= MAX_DECLS) return;
+    decls[decl_count++].proto = strdup(proto);
+}
+
+void cleanup() {
+    for (int i = 0; i < decl_count; i++) {
+        free(decls[i].proto);
+    }
+    for (int i = 0; i < handle_count; i++) {
+        dlclose(handles[i]);
+    }
+}
+
+int main() {
+    char line[4096];
+    atexit(cleanup);
+
+    while (1) {
+        printf("crepl> ");
+        fflush(stdout);
+
+        if (!fgets(line, sizeof(line), stdin)) {
+            break;
+        }
+        line[strcspn(line, "\n")] = '\0';
+
+        if (strncmp(line, "int ", 4) == 0) {
+            char *proto = NULL;
+            extract_prototype(line, &proto);
+            if (!proto) {
+                fprintf(stderr, "Error extracting prototype\n");
+                continue;
+            }
+
+            char c_path[] = TEMPLATE ".c";
+            int fd = mkstemps(c_path, 2);
+            if (fd == -1) {
+                perror("mkstemps");
+                free(proto);
+                continue;
+            }
+            write(fd, line, strlen(line));
+            close(fd);
+
+            char so_path[256];
+            size_t len = strlen(c_path);
+            if (len >= 2 && strcmp(c_path + len - 2, ".c") == 0) {
+                snprintf(so_path, sizeof(so_path), "%.*s.so", (int)(len - 2), c_path);
+            } else {
+                snprintf(so_path, sizeof(so_path), "%s.so", c_path);
+            }
+
+            if (compile_so(c_path, so_path) == 0) {
+                void *handle = dlopen(so_path, RTLD_NOW | RTLD_GLOBAL);
+                if (handle) {
+                    handles[handle_count++] = handle;
+                    add_declaration(proto);
+                    printf("OK.\n");
+                } else {
+                    fprintf(stderr, "dlopen error: %s\n", dlerror());
+                }
+            }
+            free(proto);
+            unlink(c_path);
+        } else {
+            process_expression(line);
+        }
+    }
+
+    return 0;
 }
